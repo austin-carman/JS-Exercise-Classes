25fd09d1ace33b4ebbeccf989c14a88946e20f24
Austin Carman <abcarman12@gmail.com>
2021-03-25 13:54:52 -0600

    finished tasks 1-5

--- index.js
+++ index.js
@@ -114,8 +114,23 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+    constructor(attributes){
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;
+    }
+
+    speak(){
+      return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
   }
+  
+  const practice = new Lambdasian({
+    name: 'Austin',
+    age: 29,
+    location: 'Utah'
+  });
+  console.log(practice.speak());
   
   /*
     TASK 4
@@ -131,9 +146,24 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian {
+   constructor(attributes){
+     super(attributes);
+     this.specialty = attributes.specialty;
+     this.favLanguage = attributes.favLanguage;
+     this.catchPhrase = attributes.catchPhrase;
+   }
+   
+   demo(subject){
+     return `Today we are learning about ${subject}`
+   }
 
+   grade(student, subject){
+     return `${student.name} receives a perfect score on ${subject}`
+   }
  }
+
+
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
@@ -149,8 +179,25 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
+ class Student extends Lambdasian{
+     constructor(attributes){
+       super(attributes);
+       this.previousBackground = attributes.previousBackground;
+       this.className = attributes.className;
+       this.favSubjects = attributes.favSubjects;
+     }
      
+     listSubjects(){
+       return `${this.favSubjects.toString()}`
+     }
+
+     PRAssignment(subject){
+       return `${this.name} has submitted a PR for ${subject}`
+     }
+
+     sprintChallenge(subject){
+       return `${this.name} has begun sprint challenge on ${subject}`
+     }
  }
   
   /*
